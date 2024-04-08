3b19859517f5aa7edf50150b9e108ac29a37d628
Chrstjan <ps5christian@hotmail.com>
2024-04-08 08:47:57 +0200

    Might need to change bus code

--- assets/js/bus.js
+++ assets/js/bus.js
@@ -5,6 +5,7 @@ getBusdata();
 
 setInterval(getBusdata, 60000);
 
+
 function getBusdata() {
     const apiUrl = "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&rttime&format=json&useBus=1";
     fetch(apiUrl)
