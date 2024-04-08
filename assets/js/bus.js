a2606dccf0c501a9e1e8f8eb60905b15cde403f5
Christian <ps5christian@hotmail.com>
2024-04-08 18:19:51 +0200

    #10 #16 New bus code

--- assets/js/bus.js
+++ assets/js/bus.js
@@ -5,97 +5,94 @@ getBusdata();
 
 setInterval(getBusdata, 60000);
 
-
 function getBusdata() {
-    const apiUrl = "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&rttime&format=json&useBus=1";
-    fetch(apiUrl)
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error("Error fetching bus data");
-            }
-            return res.json();
-        })
-        .then((json) => {
-            console.log(json);
-            recivedBusdata(json);
-        })
-        .catch((error) => {
-            console.log("Error", error);
-        });
-}
-
-function recivedBusdata(busData) {
-    const busDeparture = busData.MultiDepartureBoard.Departure;
-    console.log(busDeparture);
-
-    busDeparture.forEach((departure) => {
-        departure.stop = departure.stop.replace(/\([^)]*\)/g, '');
-
-        const currentTime = new Date();
-        const departureTime = new Date(departure.date + " " + departure.time);
-        const timeDifference = Math.floor((departureTime - currentTime) / 60000); // Difference in minutes
-
-        if (timeDifference > 0) {
-            let timeString = '';
-            const hours = Math.floor(timeDifference / 60);
-            const minutes = timeDifference % 60;
-
-            if (hours > 0) {
-                timeString += `${hours} time${hours > 1 ? 'r' : ''} `;
-            }
-            timeString += `${minutes} minut${minutes > 1 ? 'ter' : ''}`;
-
-            departure.timeString = timeString; // Store timeString in departure object
-        }
+  const apiUrl =
+    "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&rttime&format=json&useBus=1";
+  fetch(apiUrl)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Error fetching bus data");
+      }
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json);
+      busPlan(json);
+    })
+    .catch((error) => {
+      console.log("Error", error);
     });
-
-    // Filter out departures without timeString
-    const filteredDepartures = busDeparture.filter(departure => departure.timeString !== undefined);
-
-    //Only send the first five bus departures
-    const firstFiveDepartures = filteredDepartures.splice(0, 5);
-
-    buildBusDeparture(firstFiveDepartures); // Send the filtered departures to buildBusDeparture
 }
 
+function busPlan(busdata) {
+  console.log(busdata);
+  const container = document.getElementById("busplan");
+  container.innerHTML = "";
 
-function buildBusDeparture(busArray) {
-    // Clear previous content of busContainer
-    busContainer.innerHTML = '';
+  const sliced_data = busdata.MultiDepartureBoard.Departure.slice(0, 5);
 
-    const currentTime = new Date();
-    let busContainerElement = '';
+  const ul = document.createElement("ul");
+  const li_name = document.createElement("li");
+  li_name.innerText = "BUS";
+  const li_line = document.createElement("li");
+  li_line.innerText = "FRA";
+  const li_direction = document.createElement("li");
+  li_direction.innerText = "TIL";
+  const li_time = document.createElement("li");
+  li_time.innerText = "AFGANG";
+  ul.append(li_name, li_line, li_direction, li_time);
+  container.append(ul);
 
-    busArray.forEach((departure) => {
-        const departureTime = new Date(departure.date + " " + departure.time);
-        const timeDifference = Math.floor((departureTime - currentTime) / 60000); // Difference in minutes
+  if (sliced_data.length) {
+    sliced_data.map((value, index) => {
+      const ul = document.createElement("ul");
 
-        if (timeDifference > 0) {
-            let timeString = '';
-            const hours = Math.floor(timeDifference / 60);
-            const minutes = timeDifference % 60;
+      const li_name = document.createElement("li");
+      li_name.innerText = value.line;
 
-            if (hours > 0) {
-                timeString += `${hours} time${hours > 1 ? 'r' : ''} `;
-            }
-            timeString += `${minutes} minut${minutes > 1 ? 'ter' : ''}`;
+      const li_stop = document.createElement("li");
+      li_stop.innerText = value.stop.replace(/\([^)]*\)/g, "").toUpperCase();
 
-            busContainerElement += `
-                <span>
-                    <header>
-                        <hgroup>
-                            <h3>Linje: ${departure.line}</h3>
-                            <h3>Stop: ${departure.stop}</h3>
-                            <h3>Afgang: ${timeString}</h3>
-                        </hgroup>
-                    </header>
-                </span>`; 
-        }
+      const li_direction = document.createElement("li");
+      li_direction.innerText = value.direction
+        .replace(/\([^)]*\)/g, "")
+        .toUpperCase();
+
+      const li_time = document.createElement("li");
+      li_time.innerText = calcRemainingTime(`${value.date} ${value.time}`);
+
+      ul.append(li_name, li_stop, li_direction, li_time);
+      container.append(ul);
     });
+  }
 
-    // Append constructed HTML to busContainer
-    busContainer.innerHTML = busContainerElement;
-
-    // Assuming app is the parent container where busContainer should be appended
-    app.appendChild(busContainer);
+  setTimeout(() => busPlan(busdata), 30000);
 }
+
+const calcRemainingTime = (departure_time) => {
+  const currentTimeStamp = new Date().getTime();
+  const departureTime = departure_time.split(/[.: ]/);
+
+  const departureYear = new Date().getFullYear();
+  const departureMonth = parseInt(departureTime[1], 10) - 1;
+  const departureDay = parseInt(departureTime[0], 10);
+  const departureHours = parseInt(departureTime[3], 10);
+  const departureMinutes = parseInt(departureTime[4], 10);
+
+  const departureTimeStamp = new Date(
+    departureYear,
+    departureMonth,
+    departureDay,
+    departureHours,
+    departureMinutes
+  ).getTime();
+
+  const diff_seconds = Math.abs(
+    Math.floor((departureTimeStamp - currentTimeStamp) / 1000)
+  );
+  const diff_hours = Math.floor(diff_seconds / 3600);
+  const diff_minutes = Math.floor(diff_seconds / 60);
+  return diff_hours
+    ? `${diff_hours} t ${diff_minutes} min`
+    : `${diff_minutes} min`;
+};
